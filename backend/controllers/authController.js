0ace80fe6e75eac91863cd7ea1270363295c3d54
GThashan <124758477+tharindususl@users.noreply.github.com>
2024-10-28 11:16:49 +0530

    login and logout function created

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,81 +1,114 @@
-import { User } from '../models/user.model.js';
-import bcryptjs from 'bcryptjs';
-import { genarateTokensetcookie } from '../utils/genarateTokensetcookie.js';
-import { sendWelcomeEmail, sendverificationEmail } from '../mailtrap/emails.js';
-import MessagesApi from 'mailtrap/dist/lib/api/resources/Messages.js';
-
+import { User } from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+import { genarateTokensetcookie } from "../utils/genarateTokensetcookie.js";
+import { sendWelcomeEmail, sendverificationEmail } from "../mailtrap/emails.js";
+import MessagesApi from "mailtrap/dist/lib/api/resources/Messages.js";
 
 export const SignUp = async (req, res) => {
   const { email, name, password } = req.body;
 
-  
   if (!email || !name || !password) {
-    return res.status(400).json({ success: false, message: "All fields are required" });
+    return res
+      .status(400)
+      .json({ success: false, message: "All fields are required" });
   }
 
   const exitUser = await User.findOne({ email });
   if (exitUser) {
-    return res.status(400).json({ success: false, message: "User already exists" });
+    return res
+      .status(400)
+      .json({ success: false, message: "User already exists" });
   }
 
- 
   const hashPassword = await bcryptjs.hash(password, 10);
 
-  
-  const verificationtoken = Math.floor(100000 + Math.random() * 900000).toString(); 
-
+  const verificationtoken = Math.floor(
+    100000 + Math.random() * 900000
+  ).toString();
 
   const user = new User({
     email,
     password: hashPassword,
     name,
     verificationtoken,
-    verificationtokenExpireAt: Date.now() + 24 * 60 * 60 * 1000 
+    verificationtokenExpireAt: Date.now() + 24 * 60 * 60 * 1000,
   });
 
   await user.save();
 
-  
   genarateTokensetcookie(req, res, user._id);
-  sendverificationEmail(user.email,verificationtoken);
+  sendverificationEmail(user.email, verificationtoken);
 
-  
-  res.status(201).json({ 
-    user:{
-    ...user._doc,
-    password:null
+  res.status(201).json({
+    user: {
+      ...user._doc,
+      password: null,
     },
-    success: true, 
-    message: "User registered successfully" },
-);
+    success: true,
+    message: "User registered successfully",
+  });
 };
 
-export const Emailverification = async(req,res)=>{
+export const Emailverification = async (req, res) => {
   try {
-    const {code} = req.body;
+    const { code } = req.body;
     const exituser = await User.findOne({
-      verificationtoken : code,
-      verificationtokenExpireAt : {$gt : Date.now()}
+      verificationtoken: code,
+      verificationtokenExpireAt: { $gt: Date.now() },
     });
-    if(!exituser){
-      return res.status(200).json({success:false,message:"Invalid or exipire the token"});
+    if (!exituser) {
+      return res
+        .status(200)
+        .json({ success: false, message: "Invalid or exipire the token" });
     }
 
-    exituser.isverify= true;
+    exituser.isverify = true;
     exituser.verificationtoken = undefined;
-    exituser.verificationtokenExpireAt= undefined;
+    exituser.verificationtokenExpireAt = undefined;
 
     await exituser.save();
     await sendWelcomeEmail(exituser.email, exituser.name);
-    
-    return res.status(200).json({success:true,Messages:"Welcome Email send Succefully"});
 
-
+    return res
+      .status(200)
+      .json({ success: true, Messages: "Welcome Email send Succefully" });
   } catch (error) {
-
-    return res.status(400).json({success:false,Messages:"Internal server error"});
-    
+    return res
+      .status(400)
+      .json({ success: false, Messages: "Internal server error" });
   }
+};
 
- 
-}
+export const Login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(401).json({ success: false, message: "User not exit" });
+    }
+
+    const comparePassword = await bcryptjs.compare(password, user.password);
+    if (!comparePassword) {
+      return res
+        .status(401)
+        .json({ success: false, message: "Password is not matching" });
+    }
+    user.lastLogin = new Date();
+    await user.save();
+
+    return res.status(200).json({ success: true, message: "Login Succfully" });
+  } catch (error) {
+    console.log(error)
+    return res
+      .status(401)
+      .json({ success: false, message: "Internal server error" });
+  }
+  
+};
+
+export const Logout = (req, res) => {
+  res.clearCookie("token");
+  return res.status(200).json({ success: false, message: "Logout Succefully" });
+};
--- backend/routes/authroute.js
+++ backend/routes/authroute.js
@@ -1,9 +1,11 @@
 import express from 'express'
-import {  Emailverification, SignUp } from '../controllers/authController.js';
+import {  Emailverification, Login, Logout, SignUp } from '../controllers/authController.js';
 
 const router = express.Router();
 
 router.post('/signUp', SignUp);
+router.post('/logout',Logout);
+router.post('/login',Login)
 router.post('/verify-email',Emailverification)
 
 

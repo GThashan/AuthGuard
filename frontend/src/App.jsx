ed779f8e913b11270917ab3cf0bae11157677f23
GThashan <124758477+tharindususl@users.noreply.github.com>
2024-10-29 00:41:05 +0530

    verify email page created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import SignUp from "./pages/SignUp";
 import Login from "./pages/Login";
+import EmailVerificationPage from "./pages/EmailVerificationPage";
 
 function App() {
  
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<SignUp/>}></Route>
         <Route path="/login" element={<Login/>}></Route>
         <Route path="/register" element={<SignUp/>}></Route>
+        <Route path="/email-verify" element={<EmailVerificationPage/>}></Route>
       </Routes>
     </div>
   )

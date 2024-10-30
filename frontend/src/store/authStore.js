5edff39c807765549cf5e4ad0653040abb0c5ab4
GThashan <124758477+tharindususl@users.noreply.github.com>
2024-10-30 17:20:09 +0530

    changes in suthstore

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -9,10 +9,10 @@ export const useauthStore = create((set)=>({
     isCheckeckingauth : true,
     
 
-    signUp: async(email,name,password)=>{
+    signUp: async(name,email,password)=>{
         set({isloading:true,error:null})
         try {
-            const response = await axios.post(`http://localhost:3000/api/auth/signUp`,{email,password,name});
+            const response = await axios.post(`http://localhost:3000/api/auth/signUp`,{name,email,password});
             set({user: response.data.user , isAuthenticated:true, isloading:false})
         } catch (error) {
             set({error: error.response.data.massage || "Signing faliure" ,  isloading:false})

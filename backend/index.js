788916fb301dd1d5167e4d18181a251bcebda0fe
GThashan <124758477+tharindususl@users.noreply.github.com>
2024-11-02 08:37:24 +0530

    chage the file stucture

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import path from "path";
+
 
 import { ConnDB } from "./db/connDB.js";
 import authRoute from "./routes/authroute.js";
@@ -11,22 +11,15 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const __dirname = path.resolve();
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+
+app.use(cors());
 
 app.use(express.json()); 
 app.use(cookieParser()); 
 
 app.use("/api/auth", authRoute);
 
-if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/frontend/dist")));
-
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-	});
-}
 
 app.listen(PORT, () => {
 	ConnDB();
